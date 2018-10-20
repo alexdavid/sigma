df025f7dd8b6f9f58bf219df1257291097fcd6e5
Alex David <alex.david@originate.com>
2018-10-20 14:14:57 -0700

    Make mock client actually do something

--- mock/client.go
+++ mock/client.go
@@ -1,29 +1,65 @@
 package mock
 
-import "github.com/alexdavid/sigma"
+import (
+	"fmt"
+	"time"
+
+	"github.com/alexdavid/sigma"
+)
 
 type mockClient struct {
+	lastId int
+	chats  map[int][]sigma.Message
 }
 
 func NewClient() (sigma.Client, error) {
-	return &mockClient{}, nil
+	return &mockClient{
+		lastId: 1,
+		chats: map[int][]sigma.Message{
+			1: {},
+		},
+	}, nil
 }
 
-func (c *mockClient) Attachments(id int) ([]string, error) {
+func (c *mockClient) Attachments(messageId int) ([]string, error) {
 	return []string{}, nil
 }
 
 func (c *mockClient) Chats() ([]sigma.Chat, error) {
-	return []sigma.Chat{}, nil
+	results := []sigma.Chat{}
+	for chatId := range c.chats {
+		results = append(results, sigma.Chat{
+			Id:           chatId,
+			DisplayName:  fmt.Sprintf("Chat %d", chatId),
+			LastActivity: c.chats[chatId][len(c.chats[chatId])-1].Time,
+		})
+	}
+	return results, nil
 }
 
 func (c *mockClient) Close() {
 }
 
 func (c *mockClient) Messages(query sigma.MessagesQuery) ([]sigma.Message, error) {
-	return []sigma.Message{}, nil
+	messages, ok := c.chats[query.ChatId]
+	if !ok {
+		return []sigma.Message{}, fmt.Errorf("Chat id %d doesn't exist", query.ChatId)
+	}
+	return messages, nil
 }
 
 func (c *mockClient) SendMessage(chatId int, message string) error {
+	messages, ok := c.chats[chatId]
+	if !ok {
+		return fmt.Errorf("Chat id %d doesn't exist", chatId)
+	}
+	c.lastId++
+	c.chats[chatId] = append(messages, sigma.Message{
+		Id:        c.lastId,
+		FromMe:    true,
+		Text:      message,
+		Time:      time.Now(),
+		Delivered: true,
+	})
 	return nil
 }
