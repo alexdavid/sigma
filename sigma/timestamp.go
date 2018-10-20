2e5bebc0b11e480b5e9f4873703221c49289ee2a
Alex David <alex.david@originate.com>
2018-10-20 12:50:41 -0700

    Prevent export of epoc const

--- sigma/timestamp.go
+++ sigma/timestamp.go
@@ -3,12 +3,12 @@ package sigma
 import "time"
 
 // Cocoa epoch is Jan 1st 2001 not 1970 so add number of seconds in 31 years
-const COCOA_UNIX_EPOC_DIFF int64 = 978285600
+const cocoa_unix_epoc_diff int64 = 978285600
 
 func cocoaTimestampToTime(timestamp int) time.Time {
-	return time.Unix(int64(timestamp)+COCOA_UNIX_EPOC_DIFF, 0)
+	return time.Unix(int64(timestamp)+cocoa_unix_epoc_diff, 0)
 }
 
 func timeToCocoaTimestamp(time time.Time) int64 {
-	return time.Unix() - COCOA_UNIX_EPOC_DIFF
+	return time.Unix() - cocoa_unix_epoc_diff
 }
