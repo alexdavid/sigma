d6c08571a90abd408818d1f7bb679932a576f417
Alex David <alex.david@originate.com>
2020-03-15 18:19:51 -0700

    Add better godoc documentation on Sigma types

--- sigma.go
+++ sigma.go
@@ -7,28 +7,46 @@ import (
 
 // Client interface represents a sigma client (either real or mock)
 type Client interface {
-	Attachments(messageID int) ([]string, error)
+	// Chats returns a list of chat threads
 	Chats() ([]Chat, error)
-	Close()
+
+	// Messages returns a list of messages for a given chat id
 	Messages(chatID int, filter MessageFilter) ([]Message, error)
+
+	// SendMessage sends the given string message to the specified chat id
 	SendMessage(chatID int, message string) error
+
+	// SendMedia sends a new empty message with arbitrary data as an attachment to
+	// a given chat id
+	//
+	// `name` is used for the file name that will used on disk which is visible if
+	// the sender or receiver choose to save the image
+	//
+	// Due to current limitations the name will be prefixed with a random id
 	SendMedia(chatID int, name string, data io.Reader) error
+
+	// Attachments returns an array of file paths of attachments on the
+	// specified message id
+	Attachments(messageID int) ([]string, error)
+
+	// Close destructs the client
+	Close()
 }
 
-// Message is a single message in a chat
+// Chat represents an active conversation with another user
+type Chat struct {
+	ID           int       `json:"id"`
+	DisplayName  string    `json:"displayName"`
+	LastActivity time.Time `json:"lastActivity"`
+}
+
+// Message represents a single message in a chat
 type Message struct {
 	ID        int       `json:"id"`
 	Delivered bool      `json:"delivered"`
 	FromMe    bool      `json:"fromMe"`
 	Text      string    `json:"text"`
 	Time      time.Time `json:"time"`
-}
-
-// Chat is an active conversation with another user
-type Chat struct {
-	ID           int       `json:"id"`
-	DisplayName  string    `json:"displayName"`
-	LastActivity time.Time `json:"lastActivity"`
 }
 
 // MessageFilter is used to help paginate message results
